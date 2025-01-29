a1469c5c64dbd4255d7bd1c4d4ba87955a6abaa2
RubenV <rubenvitorino96@gmail.com>
2025-01-29 17:29:15 +0000

    Actualização de dados para SQL em SmartAsp.net

--- CircuitZone/Entities/Produto.cs
+++ CircuitZone/Entities/Produto.cs
@@ -9,14 +9,15 @@ namespace CircuitZone.Entities
 {
     public class Produto : BaseEntitity
     {
-            public string Nome { get; set; }
-            public string Descricao { get; set; }
-            public int MarcaId { get; set; }
-            public Marca Marca { get; set; }
-            public decimal Preco { get; set; }
-            public string CodigoEAN { get; set; }
-            public int QuantidadeDisponivel { get; set; }
-            public int CategoriaId { get; set; }
-            public Categoria Categoria { get; set; }
-            public List<Imagem> Imagens {get; set;}
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int MarcaId { get; set; }
+        public Marca Marca { get; set; }
+        public decimal Preco { get; set; }
+        public string CodigoEAN { get; set; }
+        public int QuantidadeDisponivel { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+        public List<Imagem> Imagens { get; set; }
+    }
 }
