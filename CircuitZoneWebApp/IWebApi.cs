c7e71861affc26b815e1b5584b7eb249921ac302
RubenV <rubenvitorino96@gmail.com>
2025-01-29 19:57:37 +0000

    Criação de WebApp em MudBlazor

---
+++ CircuitZoneWebApp/IWebApi.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Net.Http.Headers;
+
+namespace CircuitZoneWebApp
+{
+    //public interface IWebApi
+    //{
+    //    [Get("/getproducts")]
+    //    Task<List<ProductModel>> GetProducts(); // Mudar para correcto no futuro
+    //}
+}
