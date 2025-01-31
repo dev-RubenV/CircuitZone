6d60b67193459f8e37072e074cd6a34c53c0f9e0
JOÃO MACEDO <5675495@formacao.iefp.pt>
2025-01-31 17:15:11 +0000

    Endpoint Api

---
+++ CircuitZoneConsumerApi/Controllers/ProdutoController.cs
@@ -0,0 +1,26 @@
+﻿using CircuitZone.Data;
+using CircuitZone.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CircuitZoneConsumerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        protected readonly BusinessContext _businessContext;
+
+        public ProdutoController(BusinessContext businessContext)
+        {
+            _businessContext = businessContext;
+        }
+
+        [HttpGet("/produtos")]
+        public async Task<List<Produto>> GetProduto()
+        {
+            return await _businessContext.Produtos.ToListAsync();
+        }
+    }
+}
