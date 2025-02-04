e6a16477ee10ae44fd1d9295582a6d700b73f78e
JOÃO MACEDO <5675495@formacao.iefp.pt>
2025-02-04 19:08:48 +0000

    Editar produtos

--- CircuitZone.Shared/Models/ProductModel.cs
+++ CircuitZone.Shared/Models/ProductModel.cs
@@ -9,5 +9,16 @@ namespace CircuitZone.Shared.Models
     public class ProductModel
     {
         public long Id { get; set; }
+        //
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Marca { get; set; }
+        public string Categoria { get; set; }
+        public decimal Preco { get; set; }
+        public string CodigoEAN { get; set; }
+        public int QuantidadeDisponivel { get; set; }
+        public string Imagem { get; set; }
+
     }
 }
+
--- CircuitZoneConsumerApi/Controllers/ProdutoController.cs
+++ CircuitZoneConsumerApi/Controllers/ProdutoController.cs
@@ -3,6 +3,7 @@ using CircuitZone.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using CircuitZone.Shared.Models;
 
 namespace CircuitZoneConsumerApi.Controllers
 {
@@ -21,6 +22,60 @@ namespace CircuitZoneConsumerApi.Controllers
         public async Task<List<Produto>> GetProduto()
         {
             return await _businessContext.Produtos.ToListAsync();
+        }
+
+        //
+
+        [HttpPost("/addprodutos")]
+        public async Task<IActionResult> AddProdutos(ProductModel productModel)
+        {
+            var produto = await _businessContext.Produtos.FirstOrDefaultAsync(t => t.Id.Equals(productModel.Id));
+
+            if (produto is not null)
+                return BadRequest();
+
+            var newProduto = new Produto();
+            newProduto.Nome = productModel.Nome;
+            newProduto.Descricao = productModel.Descricao;
+            //newProduto.Marca = productModel.Marca;
+            //newProduto.Categoria = productModel.Categoria;
+            newProduto.Preco = productModel.Preco;
+            newProduto.CodigoEAN = productModel.CodigoEAN;
+            newProduto.QuantidadeDisponivel = productModel.QuantidadeDisponivel;
+            //newProduto.Imagem = productModel.Imagem;
+
+            _businessContext.Produtos.Add(newProduto);
+
+            var result = await _businessContext.SaveChangesAsync();
+
+            if (result.Equals(1))
+                return Ok();
+
+            return BadRequest();
+        }
+
+        [HttpPut("/editproduto")]
+        public async Task<IActionResult> EditProduto(ProductModel productModel)
+        {
+            var produto = await _businessContext.Produtos.FirstOrDefaultAsync(t => t.Id.Equals(productModel.Id));
+
+            if (produto is null)
+                return BadRequest();
+
+            produto.Nome = productModel.Nome;
+            produto.Descricao = productModel.Descricao;
+            //produto.Marca = productModel.Marca; 
+            //produto.Categoria = productModel.Categoria;
+            produto.Preco = productModel.Preco;
+            produto.QuantidadeDisponivel = productModel.QuantidadeDisponivel;
+            //produto.Imagem = productModel.Imagem;
+
+            var result = await _businessContext.SaveChangesAsync();
+
+            if (result.Equals(1))
+                return Ok();
+
+            return BadRequest();
         }
     }
 }
