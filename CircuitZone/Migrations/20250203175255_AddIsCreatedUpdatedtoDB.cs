7e67efc88012b3dd7da8d510afdb01f9664525d0
RubenV <rubenvitorino96@gmail.com>
2025-02-05 17:19:02 +0000

    Incorporação de Dark e Light Mode e modificação das páginas de produtos e adicionar produtos.

---
+++ CircuitZone/Migrations/20250203175255_AddIsCreatedUpdatedtoDB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CircuitZone.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsCreatedUpdatedtoDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "IsCreated",
+                table: "Produtos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "IsUpdated",
+                table: "Produtos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCreated",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "IsUpdated",
+                table: "Produtos");
+        }
+    }
+}
--- CircuitZoneConsumerApi/Models/ProductModel.cs
+++ CircuitZoneConsumerApi/Models/ProductModel.cs
@@ -6,12 +6,29 @@ namespace CircuitZoneConsumerApi.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public string Descricao { get; set; }
+        public string? Descricao { get; set; }
         public decimal Preco { get; set; }
         public string CodigoEAN { get; set; }
         public int QuantidadeDisponivel { get; set; }
-        public string MarcaNome { get; set; }
-        public string CategoriaNome { get; set; }
+        public string? MarcaNome { get; set; }
+        public string? CategoriaNome { get; set; }
         public List<string> ImagensUrls { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime IsCreated { get; set; }
+        public DateTime IsUpdated { get; set; }
+        public int MarcaId { get; set; }
+        public int CategoriaId { get; set; }
+    }
+
+    public class MarcasModel
+    {
+        public int Id { get; set; }
+        public string NomeMarca { get; set; }
+    }
+
+    public class CategoriasModel
+    {
+        public int Id { get; set; }
+        public string CategoriaNome { get; set; }
     }
 }
