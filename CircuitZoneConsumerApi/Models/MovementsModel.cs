42c7d001ce7f1cf9b79fcc9fed7a56edb0c940f3
RubenV <rubenvitorino96@gmail.com>
2025-02-19 20:00:12 +0000

    Criadas Páginas para entrada e saida de stock Desenvolvimento Api para movimentos

---
+++ CircuitZoneConsumerApi/Models/MovementsModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CircuitZoneConsumerApi.Models
+{
+    public class MovementsModel
+    {
+        public int Id { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime DataMovimento { get; set; }
+        public string TipoMovimento { get; set; }
+        public int TipoMovimentoId { get; set; }
+        public int ProdutoId { get; set; }
+        public string Produto { get; set; }
+    }
+}
